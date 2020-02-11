3980d5165379cedba0f898274a3e6dcb3bf29cfb
FooBarAUT <39121688+FooBarAUT@users.noreply.github.com>
2020-02-11 15:35:07 +0100

    Change: add lazy full scan mode

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from datetime import datetime
 from src import macadress
 from src import ping
+import sys
 
-start = int(input("Enter the first adress to scan: "))
-end = int(input("Enter the last adress: "))
+choice = int(
+    input("\nChoose:\n1 - full (1-254) or\n2 - partial mode (custom range)\n\n|> "))
+
+if (choice == 1):
+    results = ping.get_confirmed_adresses("1", "254")
+elif (choice == 2):
+    start = int(input("\nEnter the first adress to scan: "))
+    end = int(input("Enter the last adress: "))
+    results = ping.get_confirmed_adresses(start, end)
+else:
+    sys.exit("\nInvalid choice!\n(Is it really that hard to hit 1 or 2?\n")
+
 
 starttime = datetime.now()
-
-results = ping.get_confirmed_adresses(start, end)
 
 for ip in results:
     mac = macadress.get_mac_from_ip(ip)
