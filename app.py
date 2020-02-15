5780d3ed014e4977fb2b4a7b6bd80f2b52e2c253
FooBarAUT <39121688+FooBarAUT@users.noreply.github.com>
2020-02-15 22:40:49 +0100

    Change: format console output

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ for ip in results:
     mac = macadress.get_mac_from_ip(ip)
     hostname = macadress.get_hostname(ip)
 
-    print("\n{} has hostname '{}' and MAC adress '{}'."
-          .format(ip, hostname, mac))
+    print("\n{}\nHostname: '{}'\nMAC-adress: {}"
+          .format(ip, hostname[0], mac))
